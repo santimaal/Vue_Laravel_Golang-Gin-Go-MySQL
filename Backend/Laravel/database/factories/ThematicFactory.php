1c2113717e73ae25b8b00920372c5a1e431d692c
Vicent Esteve <esteve.ferre.vicent@gmail.com>
2022-12-04 23:20:32 +0100

    Updated factory thematics and users

--- Backend/Laravel/database/factories/ThematicFactory.php
+++ Backend/Laravel/database/factories/ThematicFactory.php
@@ -3,22 +3,85 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Thematic;
-use PhpParser\Node\Expr\ArrayItem;
 
 class ThematicFactory extends Factory
 {
-    
+    private $name = null;
     public function definition()
     {
-        $random_city=['Valencia','Madrid','Barcelona','Extremadura','Aragón','Caceres','Jerez','Jaen','Bilbao','Gijon','Sevilla','Badajoz'];
-        $city = fake()-> randomElement($random_city);
-
         return [
-            'name' => $city . ' Thematic',
-            'location' => $city,
-            'img' => $city . '.png',
+            'name' => $this->nameCity() . ' Thematic',
+            'location' => $_SESSION['NameCity'],
+            'img' => $this->imgThematic(),
+
         ];
     }
 
+    private function nameCity()
+    {
+        if ($this->name === null) {
+            $this->name = array('Valencia', 'Madrid', 'Barcelona', 'Extremadura', 'Aragón', 'Caceres', 'Jerez', 'Jaen', 'Bilbao', 'Gijon', 'Sevilla', 'Badajoz');
+        }
+
+        $city = array_pop($this->name);
+        $_SESSION['NameCity'] = $city;
+        return $city;
+    }
+    private function imgThematic()
+    {
+        $img = array(
+            "GastronomiaValenciana.png",
+            "GastronomiaMadrid.png",
+            "GastronomiaBarcelona.png",
+            "GastronomiaExtremadura.png",
+            "GastronomiaAragon.png",
+            "GastronomiaCaceres.png",
+            "GastronomiaJerez.png",
+            "GastronomiaJaen.png",
+            "GastronomiaBilbao.png",
+            "GastronomiaGijon.png",
+            "GastronomiaSevilla.png",
+            "GastronomiaBadajoz.png",
+        );
+        $image = "";
+        switch ($_SESSION['NameCity']) {
+            case "Valencia":
+                $image = $img[0];
+                break;
+            case "Madrid":
+                $image = $img[1];
+                break;
+            case "Barcelona":
+                $image = $img[2];
+                break;
+            case "Extremadura":
+                $image = $img[3];
+                break;
+            case "Aragón":
+                $image = $img[4];
+                break;
+            case "Caceres":
+                $image = $img[5];
+                break;
+            case "Jerez":
+                $image = $img[6];
+                break;
+            case "Jaen":
+                $image = $img[7];
+                break;
+            case "Bilbao":
+                $image = $img[8];
+                break;
+            case "Gijon":
+                $image = $img[9];
+                break;
+            case "Sevilla":
+                $image = $img[10];
+                break;
+            case "Badajoz":
+                $image = $img[11];
+                break;
+        }
+        return $image;
+    }
 }
--- Backend/Laravel/database/factories/UserFactory.php
+++ Backend/Laravel/database/factories/UserFactory.php
@@ -1,21 +1,31 @@
 <?php
- 
+
 namespace Database\Factories;
- 
+
 use Illuminate\Database\Eloquent\Factories\Factory;
- 
+
 class UserFactory extends Factory
 {
-   
-   public function definition()
-   {
-       return [
-           'is_active' => 0,
-           'name' => fake()->name(),
-           'password' => fake()->password(),
-           'email' => fake()->email(),
-           'type' => fake()-> randomElement(['client', 'admin'])
-       ];
-   }
-}
+    private $name = null;
+    public function definition()
+    {
+        return [
+            'is_active' => 0,
+            'name' =>  $this->namePerson(),
+            'password' => fake()->password(),
+            'email' => strtolower(explode(" ",  $_SESSION['NamePerson'])[0]). "@gmail.com",
+            'type' => fake()->randomElement(['client', 'admin'])
+        ];
+    }
 
+    private function namePerson()
+    {
+        if ($this->name === null) {
+            $name = fake()->name();
+            $_SESSION['NamePerson'] = $name;
+            return $name;
+        } else {
+            return "Ejemplo";
+        }
+    }
+}
