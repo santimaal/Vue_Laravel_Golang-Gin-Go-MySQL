736c5fe4ca930b908bd20c0e2397a834716b1403
Vicent Esteve <esteve.ferre.vicent@gmail.com>
2023-01-09 03:20:58 +0100

    Start SetProfile And more css

--- Frontend/Vue/src/components/client/Filters.vue
+++ Frontend/Vue/src/components/client/Filters.vue
@@ -1,49 +1,40 @@
 <template>
-    <form class="filters">
-        Location
-        <select name="" id="" v-model="state.filter.location">
-            <option value="outside">Outside</option>
-            <option value="inside">Inside</option>
-        </select>
-        <br>
-        Capacity
-        <input type="number" min="1" v-model="state.filter.capacity" />
-        <br>
-        Thematic
-        <select v-model="state.filter.id_thematic" class="">
-            <option v-for="(item, id) in state.thematic" :key="id" :value="item.id">{{ item.name }}</option>
-        </select>
-        <button type="button" @click="searchFilters">Search</button>
-        <button type="button" @click="resetFilters">Reset</button>
-    </form>
-
-    <!-- <div class="row justify-content-center">
+    <div class="row justify-content-center">
         <div class="col-8">
-            <form class="row filters justify-content-around bg-light rounded p-3 m-2">
-                <div class="col-md-3 position-relative">
-                    <div class="d-flex justify-content-center">
-                        <h3>Location</h3>
+            <form class="filters justify-content-around rounded p-3 m-2">
+                <div class="row justify-content-around">
+                    <div class="col-md-3 position-relative">
+                        <div class="d-flex justify-content-center">
+                            <select name="" id="" v-model="state.filter.location" class="d-flex justify-content-center">
+                                <option class="fil_center" value="" selected disabled hidden> LOCATION</option>
+                                <option class="fil_center" value="outside">Outside</option>
+                                <option class="fil_center" value="inside">Inside</option>
+                            </select>
+                        </div>
                     </div>
-                    <p class="text-center">
-                    </p>
-                </div>
-                <div class="col-md-3 position-relative">
-                    <div class="d-flex justify-content-center">
-                        <h3>Capacity</h3>
+                    <div class="col-md-3 position-relative">
+                        <div class="d-flex justify-content-center">
+                            <input class="fil_center" type="number" min="1" placeholder="CAPACITY" v-model="state.filter.capacity" />
+                        </div>
                     </div>
-                    <p class="text-center">
-                    </p>
-                </div>
-                <div class="col-md-3 position-relative">
-                    <div class="d-flex justify-content-center">
-                        <h3>Thematic</h3>
+                    <div class="col-md-3 position-relative">
+                        <div class="d-flex justify-content-center">
+                            <select v-model="state.filter.id_thematic" class="">
+                                <option class="fil_center" value="" selected disabled hidden>THMATIC</option>
+                                <option class="fil_center" v-for="(item, id) in state.thematic" :key="id" :value="item.id">{{ item.name }}
+                                </option>
+                            </select>
+                        </div>
                     </div>
-                    <p class="text-center">
-                    </p>
                 </div>
+                <div class="row justify-content-center mt-3 filbuton">
+                    <button type="button" class="mr-3" @click="searchFilters"><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></button>
+                    <button type="button" @click="resetFilters"><font-awesome-icon icon="fa-solid fa-arrows-rotate" /></button>
+                </div>
+
             </form>
         </div>
-    </div> -->
+    </div>
 
 </template>
 <script>
@@ -91,6 +82,31 @@ export default {
 </script>
 <style>
 .filters {
-    margin: 2% 2%;
+    margin-top: 5% !important;
+}
+
+select, input {
+  height: 40px;
+  width: 200px;
+  display: flex;
+  justify-content: center;
+  border-radius: 30px;
+  border: solid 2px black;
+  background-color: white;
+}
+.fil_center {
+    display: flex !important;
+    text-align: center !important;
+    border-radius: 30px !important;
+}
+input::placeholder {
+  color: black;
+}
+.filbuton button{
+    background-color: black;
+    color: white;
+    border-radius: 30px;
+    font-size: x-large;
+    
 }
 </style>
